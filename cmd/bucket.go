bd006dcb1550cdd12c50a96a672d0a63395e4c2e
Yakiyo <yakiyo1206@gmail.com>
2023-08-24 16:01:06 +0000

    feat: init add sub

--- cmd/bucket.go
+++ cmd/bucket.go
@@ -3,17 +3,41 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/Yakiyo/scoob/pkg/bucket"
+	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 )
 
-var subCmd = &cobra.Command{
+var bucketCmd = &cobra.Command{
 	Use:   "bucket",
 	Short: "Manage local buckets",
+}
+
+var bucketAdd = &cobra.Command{
+	Use:   "add",
+	Short: "Install a bucket locally",
+	Example: `scoob bucket add main
+scoob bucket add somebucket https://github.com/some/bucket`,
+	Args: cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("sub called")
+		var buckName, buckUrl string
+		if len(args) == 2 {
+			buckName = args[0]
+			buckUrl = args[1]
+		} else {
+			buckName = args[0]
+			log.Info("Attempting to find bucket url from known buckets", "bucket", buckName)
+			buckUrl = bucket.KnownBuckets[buckName]
+			if buckUrl == "" {
+				log.Error("Not a known bucket. Please provide a url to the bucket.", "bucket", buckName)
+				return
+			}
+		}
+		fmt.Println(buckName, buckUrl)
 	},
 }
 
 func init() {
-	rootCmd.AddCommand(subCmd)
+	bucketCmd.AddCommand(bucketAdd)
+	rootCmd.AddCommand(bucketCmd)
 }
