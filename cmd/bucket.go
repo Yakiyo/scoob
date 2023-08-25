9436817a8469fc0c65d16b74f8a7d8684330fc8f
Yakiyo <yakiyo1206@gmail.com>
2023-08-25 04:54:48 +0000

    feat: bucket add

--- cmd/bucket.go
+++ cmd/bucket.go
@@ -3,10 +3,11 @@ package cmd
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 
 	"github.com/Yakiyo/scoob/pkg/bucket"
+	"github.com/Yakiyo/scoob/utils"
 	"github.com/charmbracelet/log"
-	"github.com/samber/lo"
 	"github.com/spf13/cobra"
 )
 
@@ -22,6 +23,9 @@ var bucketAdd = &cobra.Command{
 scoob bucket add somebucket https://github.com/some/bucket`,
 	Args: cobra.RangeArgs(1, 2),
 	Run: func(cmd *cobra.Command, args []string) {
+		if !utils.GitExists() {
+			utils.Error("Unable to find git in path. Git is required for installing buckets")
+		}
 		var buckName, buckUrl string
 		if len(args) == 2 {
 			buckName = args[0]
@@ -31,11 +35,26 @@ scoob bucket add somebucket https://github.com/some/bucket`,
 			log.Info("Attempting to find bucket url from known buckets", "bucket", buckName)
 			buckUrl = bucket.KnownBuckets[buckName]
 			if buckUrl == "" {
-				log.Error("Not a known bucket. Please provide a url to the bucket.", "bucket", buckName)
+				utils.Error("Not a known bucket. Please provide a url to the bucket.", "bucket", buckName)
 				return
 			}
+			log.Info("Resolved bucket url from known buckets", "url", buckUrl)
 		}
-		fmt.Println(buckName, buckUrl)
+		if bucket.Exists(buckName) {
+			utils.Error("Bucket already exists. Consider uninstalling it first and then reinstalling", "bucket", buckName)
+		}
+		bucketPath := bucket.GetPath(buckName)
+		if parent := filepath.Dir(bucketPath); parent != "" && !utils.PathExists(parent) {
+			err := os.MkdirAll(parent, os.ModePerm)
+			if err != nil {
+				utils.Error("Unable to create parent dir of buckets", "err", err)
+			}
+		}
+		code, err := utils.RunGit(true, "clone", buckUrl, bucketPath)
+		if err != nil {
+			utils.Error("Failed to clone bucket, error running git", "exitCode", code, "err", err)
+		}
+		fmt.Printf("Successfully installed bucket\nName: %v\nUrl: %v\nPath: %v\n", buckName, buckUrl, bucketPath)
 	},
 }
 
@@ -45,18 +64,17 @@ var bucketList = &cobra.Command{
 	Example: "scoob bucket list",
 	Args:    cobra.NoArgs,
 	Run: func(cmd *cobra.Command, args []string) {
-		l, err := bucket.ListBucket()
+		l, err := bucket.List()
 		if err != nil {
-			log.Error("Unable to read buckets directory", "err", err)
-			os.Exit(1)
+			utils.Error("Unable to read buckets directory", "err", err)
 		}
 		if len(l) < 1 {
 			fmt.Println("No buckets installed locally")
 			return
 		}
-		lo.ForEach(l, func(it string, _ int) {
-			fmt.Println(l)
-		})
+		for _, i := range l {
+			fmt.Println(i)
+		}
 
 	},
 }
