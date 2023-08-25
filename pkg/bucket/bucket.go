4652cacc5fb48d7c0ee3881a7446e35d8630a3f9
Yakiyo <yakiyo1206@gmail.com>
2023-08-25 04:31:25 +0000

    fix: handle dir not exists

--- pkg/bucket/bucket.go
+++ pkg/bucket/bucket.go
@@ -4,13 +4,18 @@ import (
 	"io/fs"
 	"os"
 
+	"github.com/Yakiyo/scoob/utils"
 	"github.com/Yakiyo/scoob/where"
 	"github.com/samber/lo"
 )
 
 // list all local buckets
 func ListBucket() ([]string, error) {
-	entries, err := os.ReadDir(where.Buckets())
+	bucketPath := where.Buckets()
+	if !utils.PathExists(bucketPath) {
+		return []string{}, nil
+	}
+	entries, err := os.ReadDir(bucketPath)
 	if err != nil {
 		return []string{}, err
 	}
