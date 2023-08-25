7e5ce06ceaacf2b3144e601fe58940a1b595e127
Yakiyo <yakiyo1206@gmail.com>
2023-08-25 04:38:17 +0000

    feat: more bucket utils

--- pkg/bucket/bucket.go
+++ pkg/bucket/bucket.go
@@ -3,6 +3,7 @@ package bucket
 import (
 	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/Yakiyo/scoob/utils"
 	"github.com/Yakiyo/scoob/where"
@@ -10,7 +11,7 @@ import (
 )
 
 // list all local buckets
-func ListBucket() ([]string, error) {
+func List() ([]string, error) {
 	bucketPath := where.Buckets()
 	if !utils.PathExists(bucketPath) {
 		return []string{}, nil
@@ -25,4 +26,14 @@ func ListBucket() ([]string, error) {
 		}
 		return "", false
 	}), nil
+}
+
+// get the path to a bucket
+func GetPath(name string) string {
+	return filepath.Join(where.Buckets(), name)
+}
+
+// wether a bucket with that name exists
+func Exists(name string) bool {
+	return utils.PathExists(GetPath(name))
 }
