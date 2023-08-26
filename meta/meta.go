748042b221452b257d8dacf8e9f43cf5e1781c80
Yakiyo <yakiyo1206@gmail.com>
2023-08-26 02:56:52 +0000

    feat: use build tags and only use binaries

--- meta/meta.go
+++ meta/meta.go
@@ -1,9 +1,15 @@
 package meta
 
-const (
+var (
 	// name of app
 	AppName = "scoob"
 
 	// app version
 	Version = "0.1.0"
+
+	// Time when it was built at
+	BuiltAt = "Unset"
+
+	// Git commit
+	Revision = "Unset"
 )
