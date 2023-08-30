652e5ce3f33be5165500dc758a2544007bb43e88
Yakiyo <yakiyo1206@gmail.com>
2023-08-30 15:24:07 +0000

    chore: inpromptu commit

--- internal/manifest/manifest.go
+++ internal/manifest/manifest.go
@@ -1,11 +1,25 @@
 package manifest
 
+import (
+	"errors"
+	"fmt"
+	"reflect"
+
+	json "github.com/json-iterator/go"
+)
+
 // A struct representing a scoob manifest
 type Manifest struct {
-	Version     string `json:"version"`
-	Url         string `json:"url"`
-	Description string `json:"description"`
-	License     string `json:"License"`
+	Version      string              `json:"version"`
+	Url          string              `json:"url"`
+	Description  string              `json:"description"`
+	License      License             `json:"License"`
+	Architecture map[string]ArchInfo `json:"architecture"`
+	Env_add_path []string            `json:"env_add_path"`
+	Bin          Bin                 `json:"bin"`
+	Hash         Hash                `json:"hash"`
+	Extract_dir  string              `json:"extract_dir"`
+	Extract_to   string              `json:"extract_to"`
 }
 
 // a license struct
@@ -17,9 +31,52 @@ type License struct {
 }
 
 // info specific to an architecture
-type ArchitectureInfo struct {
+type ArchInfo struct {
 	Url         string `json:"url"`
-	Hash        string `json:"hash"`
+	Hash        Hash   `json:"hash"`
 	Extract_dir string `json:"extract_dir"`
 	Extract_to  string `json:"extract_to"`
 }
+
+// The bin key
+// this can be one of the following:
+//   - a string
+//   - a slice of strings
+//   - a slice of (slice of strings)
+type Bin [][]string
+
+func (b *Bin) UnmarshalJSON(data []byte) error {
+	fmt.Println(string(data))
+	if len(data) < 1 {
+		return errors.New("required key `bin` is empty, must be an array of string arrays or a string")
+	}
+	var v [][]string
+	var i interface{}
+	err := json.Unmarshal(data, &i)
+	if err != nil {
+		return err
+	}
+	fmt.Println(reflect.TypeOf(i))
+	switch d := i.(type) {
+	case string:
+		v = [][]string{{d}}
+	case []string:
+		for _, k := range d {
+			v = append(v, []string{k})
+		}
+	case [][]string:
+		v = d
+	default:
+		return errors.New("invalid value for bin received, must be one of string, []string or [][]string")
+	}
+	if len(v) < 1 {
+		return errors.New("did not received any value for `bin`")
+	}
+	//lint:ignore SA4006 dont care
+	t := Bin(v)
+	b = &t
+	return nil
+}
+
+// can be a string or a slice of strings
+type Hash []string
---
+++ internal/manifest/manifest_test.go
@@ -0,0 +1,22 @@
+package manifest
+
+import (
+	"fmt"
+	"testing"
+
+	json "github.com/json-iterator/go"
+)
+
+func TestParse(t *testing.T) {
+	value := `{
+		"version": "1.0.0",
+		"url": "https://github.com/Yakiyo/scoob/releases/latest/download/blah-blah.zip",
+		"bin": "scoob.exe"
+	}`
+	mf := Manifest{}
+	err := json.UnmarshalFromString(value, &mf)
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%#v\n", mf.Bin)
+}
