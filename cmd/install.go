fb5f799d5facd547bf9fba0564b46d85803c7cac
Yakiyo <yakiyo1206@gmail.com>
2023-08-31 12:52:17 +0000

    chore: some more work

--- cmd/install.go
+++ cmd/install.go
@@ -1,9 +1,16 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"regexp"
+
+	"github.com/Yakiyo/scoob/utils"
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+)
 
 var installCmd = &cobra.Command{
-	Use: "install",
+	Use:   "install",
 	Short: "Install an app",
 	Long: `Install apps via scoob
 Help: e.g. The usual way to install an app (uses your local 'buckets'):
@@ -18,11 +25,26 @@ To install an app from a manifest at a URL:
 
 To install an app from a manifest on your computer
     scoob install \path\to\app.json`,
-	Args: cobra.ExactArgs(1), // TODO: support installing multiple apps in the future
-	Aliases: []string{"add"},
+	Args:         cobra.ExactArgs(1), // TODO: support installing multiple apps in the future
+	Aliases:      []string{"add"},
 	SilenceUsage: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		var arch string
+		if fv := lo.Must1(cmd.Flags().GetString("arch")); fv != "" {
+			arch = fv
+		} else {
+			arch = lo.Must1(utils.GetDefaultArch())
+		}
+		_ = arch
 
+		//lint:ignore SA9003 todo
+		if !lo.Must1(cmd.Flags().GetBool("no-update")) {
+			// TODO: update scoop
+		}
+		_, err := parseApp(args[0])
+		if err != nil {
+			return err
+		}
 		return nil
 	},
 }
@@ -36,4 +58,26 @@ func init() {
 	f.BoolP("skip", "s", false, "Skip hash validation (use with caution!)")
 	f.StringP("arch", "a", "", "Use the specified architecture, if the app supports it (32bit|64bit|arm64)")
 	rootCmd.AddCommand(installCmd)
+}
+
+// regular expression to match an app argument
+var appRgx = regexp.MustCompile(`^(?:(?P<bucket>[a-zA-Z0-9-_.]+)/)?(?P<app>.*\.json$|[a-zA-Z0-9-_.]+)(?:@(?P<version>.*))?$`)
+
+// parse an app argument
+func parseApp(app string) (parsedApp, error) {
+	matches := appRgx.FindStringSubmatch(app)
+	p := parsedApp{}
+	if matches == nil || len(matches) <= 1 {
+		return p, fmt.Errorf("Arg %v did not match valid app name syntax, must match `bucket/name@version` format", app)
+	}
+	p.Name = matches[2]
+	p.Bucket = matches[1]
+	p.Version = matches[3]
+	return p, nil
+}
+
+type parsedApp struct {
+	Name    string
+	Bucket  string
+	Version string
 }
