c875653869c0a66e0538a134a30e51757144ff5c
Yakiyo <yakiyo1206@gmail.com>
2023-08-31 11:21:54 +0000

    feat: installer

--- pkg/manifest/installer.go
+++ pkg/manifest/installer.go
@@ -1,3 +1,9 @@
 package manifest
 
-type Installer struct{}
+// The `installer` used in Installer key and Uninstaller
+type Installer struct {
+	File   string             `json:"file"` // for installer, defaults to url, required for uninstaller
+	Script Vectorized[string] `json:"script"`
+	Args   Vectorized[string] `json:"args"`
+	Keep   string             `json:"keep"` // "true" (string, not bool) to keep file, else remove. Ignored for uninstaller
+}
