aeb0ed757268ae698188a03c347b1eaede9de680
Yakiyo <yakiyo1206@gmail.com>
2023-09-03 12:08:23 +0000

    chore: impromptu commit

--- pkg/app/install.go
+++ pkg/app/install.go
@@ -1,7 +1,9 @@
 package app
 
+import "github.com/Yakiyo/scoob/pkg/manifest"
+
 // install an app
-func Install() error {
+func Install(app ParsedApp, manifest manifest.Manifest, global bool, use_cache bool, check_hash bool) error {
 
 	return nil
 }
