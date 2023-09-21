663fc84c2b422ceb95a375d677c2895bf3665e9a
Yakiyo <yakiyo1206@gmail.com>
2023-09-21 16:20:38 +0000

    feat: set default value for arch

--- utils/config/init.go
+++ utils/config/init.go
@@ -1,9 +1,14 @@
 package config
 
 import (
+	"path/filepath"
+
+	"github.com/Yakiyo/scoob/utils"
 	"github.com/Yakiyo/scoob/utils/meta"
 	"github.com/Yakiyo/scoob/utils/where"
 	"github.com/charmbracelet/log"
+	homedir "github.com/mitchellh/go-homedir"
+	"github.com/samber/lo"
 	v "github.com/spf13/viper"
 )
 
@@ -12,17 +17,19 @@ func init() {
 	v.SetConfigName(meta.AppName)
 	v.SetConfigType("toml")
 
-	// search in default directory
-	v.AddConfigPath(where.RootDir())
+	// search in default directory - ~/.config/
+	v.AddConfigPath(filepath.Join(lo.Must(homedir.Dir()), ".config"))
 
 	// add default values here
 	v.SetDefault("log_level", "warn")
 	v.SetDefault("color", "auto")
-	v.SetDefault("root_dir", where.RootDir())
+	v.SetDefault("root_path", where.RootDir())
+	v.SetDefault("default_architecture", lo.Must(utils.GetDefaultArch()))
 }
 
 // read config file
 func Read() {
+
 	if err := v.ReadInConfig(); err != nil {
 		if _, ok := err.(v.ConfigFileNotFoundError); ok {
 			log.Info("Missing log file in default location. Using defaults")
